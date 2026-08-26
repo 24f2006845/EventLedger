56ba5dcd93c2572a8afeb7dfb0549520173a83f4
Haider <Haiderkg78@gmail.com>
2026-08-27 00:38:54 +0530

    fix: correct project routes by adding missing controller functions

--- Backend/src/modules/Projects/project.routes.ts
+++ Backend/src/modules/Projects/project.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authMiddleware } from '../../middlewares/auth.middleware.js';
+import { createProjectController, deleteProjectController, getAllProjectsController, getProjectByIdController } from './project.controller.js';
 const router = express.Router();
 
 
-router.get('/', authMiddleware)
-router.post('/create', authMiddleware)
-router.get('/:id', authMiddleware)
-router.delete('/:id', authMiddleware)
+router.get('/', authMiddleware , getAllProjectsController);
+router.post('/create', authMiddleware , createProjectController);
+router.get('/:id', authMiddleware , getProjectByIdController);
+router.delete('/:id', authMiddleware , deleteProjectController);
 
 export default router;
