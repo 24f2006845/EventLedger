594d30fe7679ba02f358aeed0d8fc34a7eb55949
Haider <Haiderkg78@gmail.com>
2026-08-26 23:54:52 +0530

    project route setup

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,10 +3,12 @@ import cookieParser from 'cookie-parser';
 const app = express();
 import authRoutes from './modules/auth/auth.routes.js';
 import apiKeyRoutes from './modules/apiKey/apiKey.routes.js';
+import projectRoutes from './modules/Projects/project.routes.js';
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/apiKeys', apiKeyRoutes);
+app.use('/api/projects', projectRoutes);
 
 export default app;
---
+++ Backend/src/modules/Projects/project.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/')
+router.post('/create')
+router.get('/:id')
+router.delete('/:id')
+
+export default router;
