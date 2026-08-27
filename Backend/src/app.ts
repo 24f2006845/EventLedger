1c4b5a75b86c7f1cb1bca236a26607d72bdcc2cf
Haider <Haiderkg78@gmail.com>
2026-08-28 02:30:31 +0530

    feat: add admin module with routes, controller, service, types, and validation

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -4,11 +4,13 @@ const app = express();
 import authRoutes from './modules/auth/auth.routes.js';
 import apiKeyRoutes from './modules/apiKey/apiKey.routes.js';
 import projectRoutes from './modules/Projects/project.routes.js';
+import AdminRoutes from './modules/admin/admin.routes.js';
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/project/:projectId/apiKey', apiKeyRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/admin', AdminRoutes);
 
 export default app;
