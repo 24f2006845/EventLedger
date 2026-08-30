a17f03e307aa170e124ab00b3dddc30717e6b85d
Haider <Haiderkg78@gmail.com>
2026-08-30 20:45:08 +0530

    creatting the admin routes

--- Backend/src/modules/admin/admin.routes.ts
+++ Backend/src/modules/admin/admin.routes.ts
@@ -1,3 +1,10 @@
 import { Router } from "express";
 const router = Router();
+ 
+
+router.get('/users',)
+router.get('/roles',)
+router.get('/projects',)
+router.get('/apiKeys',)
+
 export default router;
