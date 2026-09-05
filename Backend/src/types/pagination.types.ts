70df8f01526f5dbcac05337faaf20b49a89aa26f
Haider <Haiderkg78@gmail.com>
2026-09-06 01:02:28 +0530

    paginate meta data and setup about pagiante response

--- Backend/src/types/pagination.types.ts
+++ Backend/src/types/pagination.types.ts
@@ -1,5 +1,10 @@
-export interface PaginationResponse<T> {
-    result : T[];
-    nextCursor: string | null;
-    hasMore: boolean;
+export interface PaginationMeta {
+  limit: number;
+  hasMore: boolean;
+  nextCursor: string | null;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  pagination: PaginationMeta;
 }
