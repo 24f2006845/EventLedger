02de37aeb2acd6f48d1604df2734635409723272
Haider <Haiderkg78@gmail.com>
2026-09-07 00:31:28 +0530

    little change

--- Backend/src/validations/pagination.validation.ts
+++ Backend/src/validations/pagination.validation.ts
@@ -1,6 +1,7 @@
 import * as z from "zod";
 
 export const PaginationSchema = z.object({
-  limit: z.number().int().positive().min(1).max(100).default(20),
+  limit: z.coerce
+  .number().int().min(1).max(100).default(20),
   cursor: z.string().optional(),
 });
